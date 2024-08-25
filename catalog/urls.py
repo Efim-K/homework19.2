6e71d147abf86c0263c95553afec8e54f47447d4
Efim Kashapov <bambalabes@gmail.com>
2024-08-25 17:21:50 +0500

    Доработан блок с контентом продукта, Добавлена страница с контактами Обновлено наполнение данными продуктами

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, product_detail
+from catalog.views import product_list, product_detail, contacts
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", product_list, name="product_list"),
     path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("contacts/", contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
+
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, "products_list.html", context)
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -13,14 +15,10 @@ def product_detail(request, pk):
     return render(request, "product_detail.html", context)
 
 
-# def home(request):
-#     return render(request, "home.html")
-#
-#
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#         print(f"{name} \n{phone} \n{message}\n")
-#     return render(request, "contacts.html")
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}\n{phone}\n{message}')
+    return render(request, 'contacts.html')
