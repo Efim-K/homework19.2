571985106d77975dd4c238358a85cc6ef9feeef5
Efim Kashapov <bambalabes@gmail.com>
2024-09-02 19:16:45 +0500

    Создание админ панели блога

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Blog
+
 
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("id", 'title', 'slug', 'content', 'preview', 'is_published',)
+    search_fields = (
+        "title",
+        "content",
+    )
