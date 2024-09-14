c7ac9330d297aef6582e239e0ad9c59f83c94cda
Efim Kashapov <bambalabes@gmail.com>
2024-09-14 13:34:05 +0500

    Добавлена валидация названия и описания продукта

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 
 from catalog.models import Product
 
@@ -7,3 +8,21 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         exclude = ('created_at', 'updated_at',)
+
+    def clean_name(self):
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        name = self.cleaned_data['name']
+        for word in forbidden_words:
+            if word in name.lower():
+                raise ValidationError(f'Имя продукта не должно содержать запрещенное слово: {word}')
+        return name
+
+    def clean_depiction(self):
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        depiction = self.cleaned_data['depiction']
+        for word in forbidden_words:
+            if word in depiction.lower():
+                raise ValidationError(f'Описание продукта не должно содержать запрещенное слово: {word}')
+        return depiction
