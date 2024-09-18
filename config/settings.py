b2d1ea89df71cc9306b76ad0b0a91128aba71183
Efim Kashapov <bambalabes@gmail.com>
2024-09-18 18:51:18 +0500

    Добавлена модель пользователя

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR.joinpath("media")
+
+AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
---
+++ users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from catalog.models import NULLABLE
+
+
+# Create your models here.
+class User(AbstractUser):
+    """
+    Пользователь с полями имя, фамилия, email, пароль и дата регистрации.
+    """
+    name = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    avatar = models.ImageField(upload_to="users/avatars", verbose_name="Аватар", **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='Phone', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='Country', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
