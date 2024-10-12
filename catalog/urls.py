eeec28339aea448cc41493bccb36487cd308cc59
Efim Kashapov <bambalabes@gmail.com>
2024-10-12 14:23:38 +0500

    кэширование одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, contacts, ProductCreateView, ProductDeleteView, \
@@ -7,7 +8,7 @@ from catalog.views import ProductListView, ProductDetailView, contacts, ProductC
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:pk>/", cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
     path("contacts/", contacts, name="contacts"),
     path("create/", ProductCreateView.as_view(), name="product_create"),
     path("delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
